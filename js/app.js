3ef7bc08c650bea512ee1b3a1b6779d970700a76
unknown <islam15-3879@diu.edu.bd>
2022-02-13 16:51:10 +0600

    added

--- js/app.js
+++ js/app.js
@@ -1,17 +1,41 @@
-function getPin(){
+function getPin() {
 
-    const pin = Math.round(Math.random()*10000);
-    const pinString =pin +'';
-    if(pinString.length==4){
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + '';
+    if (pinString.length == 4) {
         return pin;
-    }
-    else{
+    } else {
         return getPin();
+
     }
-}
-function generatePin(){
-    const pin=getPin();
-  
+};
+
+function generatePin() {
+    const pin = getPin();
+
     // console.log(pin);
-    document.getElementById('display-input').value =pin;
-}
+    document.getElementById('display-input').value = pin;
+};
+
+
+// calculator er jonno re vai 
+
+document.getElementById('key-pad').addEventListener('click', function (event) {
+
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('typed-numbers');
+
+
+    if (isNaN(number)) {
+        if(number=='C'){
+            calcInput.value='';
+            console.log('ami hotash');
+        }
+    } 
+    else {
+        const previousInput = calcInput.value;
+        const newCalc = previousInput + number;
+        calcInput.value = newCalc;
+        console.log('ami hotash');
+    }
+});
